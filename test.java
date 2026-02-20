9e1bb80be443518b98645f03b047920bf6b2404e
V-Sharmila-Packiavathy <sharmila.pdkv@gmail.com>
2026-02-20 06:12:18 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Welcome All");
+            System.out.println("Good morning to All");
         }
     }
 }
